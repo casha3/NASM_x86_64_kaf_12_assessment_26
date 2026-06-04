fa0eefef99f92df5b06508ac41c5c92f55bb1622
casha3 <fedor2006new@gmail.com>
2026-06-04 19:32:59 +0300

    solution:remove_spec_char

---
+++ string_real_kr/dumb_remove_spec_char.asm
@@ -0,0 +1,94 @@
+;ostavit v stroke only delims , digits and letters
+;ostalnoye delete
+bits 64
+SYS_READ equ 0
+SYS_WRITE equ 1
+SYS_EXIT equ 60
+STDIN equ 0
+STDOUT equ 1
+section .bss
+	buf1 resb 82
+	buf2 resb 82
+section .text   
+	global _start
+_start:
+	mov rax , SYS_READ
+	mov rdi , STDIN
+	mov rsi , buf1
+	mov rdx , 81 
+	syscall
+	dec rax
+	; rax = len = amount of readen bytes = sys_read()
+	mov rbx , rax
+	; --------
+	; two pointers algorithm ( r12 <= rcx ) 
+	xor rcx ,rcx
+	xor r12 , r12
+	; ---------
+	xor r9 , r9 ; r9 - pointer on new buf2
+	.loop:
+		cmp rcx,rbx
+		jge .keep_done
+		cmp byte[buf1+rcx] , ` `
+		je .OK
+		cmp byte[buf1+rcx], `\t`
+		jne .good_forwarding_rcx
+		.OK:
+			mov dl , byte[buf1+rcx]
+			mov byte[buf2+r9] , dl
+			inc r9
+			inc rcx
+			inc r12
+		.good_forwarding_rcx:
+			cmp rcx,rbx
+			jge .good_forwarding_r12
+			mov dl , byte[buf1+rcx]
+			cmp dl , ` `
+			je .good_forwarding_r12
+			cmp dl , `\t`
+			je .good_forwarding_r12
+			cmp dl , `a`
+			jl .YELLOW_FLAG
+			cmp dl , `z`
+			jg .bad_forwarding_rcx
+			inc rcx
+			jmp .good_forwarding_rcx
+			.YELLOW_FLAG:
+				cmp dl , `A`
+				jl .bad_forwarding_rcx
+				cmp dl , `Z`
+				jg .bad_forwarding_rcx
+				inc rcx
+				jmp .good_forwarding_rcx
+		.bad_forwarding_rcx:
+			cmp rcx,rbx
+			jge .bad_forwarding_r12
+			mov dl , byte[buf1+rcx]
+			cmp dl , ` `
+			je .bad_forwarding_r12
+			cmp dl , `\t`
+			je .bad_forwarding_r12
+			inc rcx
+			jmp .bad_forwarding_rcx
+		.good_forwarding_r12:
+			; dumb realization
+			; mozhno zamenit instructions like movsb
+			cmp r12,rcx
+			jge .loop
+			mov dl , byte[buf1+r12]
+			mov byte[buf2+r9] , dl
+			inc r9
+			inc r12
+			jmp .good_forwarding_r12
+		.bad_forwarding_r12:
+			mov r12,rcx
+			jmp .loop
+	.keep_done:
+		mov rax , SYS_WRITE 
+		mov rdi , STDOUT 
+		mov rsi , buf2   
+		mov rdx , r12
+		syscall
+		mov rax , SYS_EXIT
+		xor rdi , rdi
+		syscall

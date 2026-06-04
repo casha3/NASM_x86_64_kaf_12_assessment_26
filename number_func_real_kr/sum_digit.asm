acc03a37e40786277f6c18607d6b4ce41ec49488
casha3 <fedor2006new@gmail.com>
2026-06-04 16:59:46 +0300

    solution:sum_digit

--- number_func_real_kr/sum_digit.asm
+++ number_func_real_kr/sum_digit.asm
@@ -1,29 +1,25 @@
-global f_sum_digits
-f_sum_digits
-	mov eax ,edi    
-	mov r8d,edi
+global f
+f:
+	mov eax ,edi
 	cmp eax,0
-	jge init
-		neg r8d
+	jge .init
+		neg eax
 	.init:
-        	xor r9d,r9d
+		mov ecx,eax
+		xor r8d,r8d	
 		cmp eax , 0
-		jne sum_loop
-		ret
+		je .done
 	.loop: 
 		cmp eax,0
-		je .restore_sign
+		je .restore_eax
 		mov eax,ecx ; mladshie razryady delimogo
 		xor edx,edx ; starshie razryday delimogo
 		mov ecx,10
 		div ecx
 		mov ecx,eax ; chastnoe
 		add r8d,edx ;remainder
-		jmp loop
-	.restore_sign:
-		mov eax, r9d
-		cmp r8d,0
-		jge .done
-		neg eax
+		jmp .loop
+	.restore_eax:
+		mov eax, r8d
 	.done:
 		ret
